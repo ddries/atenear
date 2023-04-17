3ed313ee73ed0b805be8b6f31089a820e7219f62
ddries <driescode@gmail.com>
2023-04-17 19:10:07 +0200

    1.6 some refactor and comments

--- background.js
+++ background.js
@@ -4,11 +4,20 @@ async function func() {
 
     let to, tr
     const b = "https://atenea.upc.edu/lib/ajax/service.php"
+
+    /* In seconds, threshold to trigger session update */
+    /* Max value = 3600 */
     const th = 3600 / 2
 
+    /* In seconds, session update delay */
+    /* Max value = 3600 */
+    const ri = 15 * 60
+
+    /* Get session time remaining */
+    /* Current session must be valid */
     const core_session_time_remaining = async () => {
         try {
-            const r = await (await fetch(b + "?sesskey=" + k + "&nosessionupdate=true", {
+            const r = await (await fetch(b + "?sesskey=" + k, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
@@ -22,15 +31,17 @@ async function func() {
                 }])
             })).json()
     
-            return r[0].data.timeremaining;
+            return r[0].data.timeremaining
         } catch(e) {
             return 0
         }
     }
 
+    /* Update current session */
+    /* Current session must be valid */
     const core_session_touch = async () => {
         try {
-            await fetch(b + "?sesskey=" + k, {
+            const r = await (await fetch(b + "?sesskey=" + k, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
@@ -42,40 +53,18 @@ async function func() {
                     methodname: "core_session_touch",
                     args: {}
                 }])
-            })
+            })).json()
+            return { error: r[0].error }
         } catch(e) {}
     }
 
-    const log = (t) => { console.log("atenear > " + t) }
-
-    const loop = async () => {
-        const t = await core_session_time_remaining()
-        tr = t - th
-
-        if (to) {
-            log("removing previous timeout...")
-            clearTimeout(to)
-        }
-
-        log("updating session in " + tr)
-
-        if (tr < th) {
-            log("updating session...")
-            await core_session_touch()
-
-            loop()
-        } else {
-            to = setTimeout(async () => {
-                log("updating session...")
-                await core_session_touch()
-                loop()
-            }, tr * 1000)
-        }
-
+    /* Inject active status indicator to Atenea */
+    const injectActiveStatus = () => {
         let ul = document.querySelector("#page-wrapper > nav > div.ml-auto.row > ul.nav.navbar-nav.flex-row-reverse.flex-md-row")
 
         if (!ul.innerHTML.includes("Atenear")) {
             let ind = document.createElement("div")
+            ind.id = 'atenear-id'
         
             ind.innerHTML = "<div style=\"width:10px;height:10px;background-color:green;border-radius:100%;\"></div><span style=\"text-decoration:underline;\">Atenear active</span>"
             ind.style = "display:flex;justify-content:center;align-items:center;gap:3px;"
@@ -84,10 +73,40 @@ async function func() {
         }
     }
 
+    /* Inject inactive status indicator to Atenea */
+    const injectInactiveStatus = () => {
+        let ul = document.querySelector("#page-wrapper > nav > div.ml-auto.row > ul.nav.navbar-nav.flex-row-reverse.flex-md-row")
+
+        if (ul.innerHTML.includes("Atenear")) {
+            document.getElementById("atenear-id").remove()
+        }
+
+        let ind = document.createElement("div")
+        ind.id = 'atenear-id'
+        
+        ind.innerHTML = "<div style=\"width:10px;height:10px;background-color:red;border-radius:100%;\"></div><span style=\"text-decoration:underline;\">Atenear OFF, refresh Atenea</span>"
+        ind.style = "display:flex;justify-content:center;align-items:center;gap:3px;"
+
+        ul.prepend(ind)
+    }
+
+    /* Refresh session wrapper to update status indicator */
+    const core_session_touch_wrapper = async () => {
+        if ((await core_session_touch()).error) {
+            injectInactiveStatus()
+        } else {
+            injectActiveStatus()
+        }
+    }
+
+    const log = (t) => { console.log("atenear > " + t) }
+
     log("injected")
     log("got auth c=" + c + ", k=" + k)
 
     const t = await core_session_time_remaining()
+
+    /* Current session is already expired */
     if (t <= 0) {
         c = ""
         k = ""
@@ -96,15 +115,20 @@ async function func() {
 
     log("remaining_time=" + t)
 
-    if (t < th) {
+    if (t < 7200) {
         log("below threshold, updating...")
-        await core_session_touch()
+        await core_session_touch_wrapper()
     }
 
-    log("launching loop...")
-    loop()
+    log("safe session age value")
+    injectActiveStatus()
 
-    return { k, c }
+    /* At this point, we can be sure we are in a safe session age value */
+    /* Create an interval which will trigger session update at given rate */
+    to = setInterval(async () => {
+        log("updating session...")
+        await core_session_touch_wrapper()
+    }, ri * 1000);
 }
 
 chrome.runtime.onMessage.addListener(async function(req, sender, sendResponse) {
